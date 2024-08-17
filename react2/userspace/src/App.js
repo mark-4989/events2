d12f564e1cb767423c4379253f609420ae30f7c5
mark <ngarimark27@gmail.com>
2024-08-18 00:06:57 +0300

    restarted th project

---
+++ react2/userspace/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbars from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+  <Navbars/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react2/userspace/src/components/navbar.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa6";
+function Navbar(){
+const[Open,setOpen]=useState(false)
+const toggleMenu =()=>{
+    setOpen(!Open)
+}
+
+    return(
+        <nav>
+            <div className="logo">
+            <h2>LOGO</h2>
+            </div>
+            <ul className={Open ? "navigation active" :"navigation"}>
+             
+                <li><a href="/">HOME</a></li>
+                <li><a href="about">ABOUT</a></li>
+                <li><a href="project">PROJECT</a></li>
+                <li><a href="contact">CONTACT</a></li>
+            </ul>
+            <div className="icons" onClick={toggleMenu}>
+            <FaBars />
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
