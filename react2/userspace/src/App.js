7bba9e4245291de48f9b428c4e94cf20d70ffe42
mark <ngarimark27@gmail.com>
2024-08-18 16:35:44 +0300

    carousel

--- react2/userspace/src/App.js
+++ react2/userspace/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbars from './components/navbar';
+import Home from './components/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
   <Navbars/>
+  <Home/>
     </div>
   );
 }
---
+++ react2/userspace/src/components/home.js
@@ -0,0 +1,51 @@
+import Carousel from 'react-bootstrap/Carousel';
+import pic1 from '../images/pexels-joshsorenson-976866.jpg';
+import pic2 from '../images/pexels-asadphoto-169198.jpg'
+import pic3 from '../images/pexels-asadphoto-1024968.jpg'
+
+function Home() {
+  return (
+    <Carousel data-bs-theme="dark" className='carousel'>
+      <Carousel.Item>
+        <img
+        style={{height:'90vh'}}
+          className="d-block w-100"
+          src={pic1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+  style={{height:'90vh'}}
+          className="d-block w-100"
+          src={pic2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          style={{height:'90vh'}}
+          className="d-block w-100"
+          src={pic3}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Home;
