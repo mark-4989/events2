9b6e22ff640eed6b39e4e790435356f4032e4e30
mark <ngarimark27@gmail.com>
2024-08-21 00:42:44 +0300

    created connection to db

--- node2/index.js
+++ node2/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-
+mongoose.connect("mongodb://127.0.0.1:27017/events")
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
