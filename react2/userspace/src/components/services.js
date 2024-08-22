078513f7d1af2fbe3187ce12f76c0bd694efd24c
mark <ngarimark27@gmail.com>
2024-08-22 19:02:11 +0300

    styled div services

--- react2/userspace/src/components/services.js
+++ react2/userspace/src/components/services.js
@@ -1,16 +1,17 @@
 import React from "react";
+// import './App.css';
 import pic2 from '../images/pexels-asadphoto-169198.jpg'
 
 function Services (){
     return(
-    <div>
-<div>
-    <div>
+    <div className="servicess">
+<div className="service">
+    <div className="wed1">
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.Lorem ipsum dolor sit amet, <br></br>consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br>Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.
         </p>
     </div>
-    <div>
+    <div className="wed2">
         <img src={pic2} ></img>
     </div>
 </div>
