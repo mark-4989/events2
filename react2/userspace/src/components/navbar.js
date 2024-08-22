12af9f872b051213a6f839abd06bac71cef953a2
mark <ngarimark27@gmail.com>
2024-08-22 17:55:46 +0300

    created services file

--- react2/userspace/src/components/navbar.js
+++ react2/userspace/src/components/navbar.js
@@ -15,6 +15,7 @@ const toggleMenu =()=>{
              
                 <li><a href="/">HOME</a></li>
                 <li><a href="about">ABOUT</a></li>
+                <li><a href="services">SERVICES</a></li>
                 <li><a href="gallery">GALLERY</a></li>
                 <li><a href="contact">CONTACT</a></li>
             </ul>
