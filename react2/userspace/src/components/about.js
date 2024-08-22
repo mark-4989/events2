ee5ce004654e7552fd61700d70e7844c8d575cc5
mark <ngarimark27@gmail.com>
2024-08-22 23:40:10 +0300

    created 1st card in about

--- react2/userspace/src/components/about.js
+++ react2/userspace/src/components/about.js
@@ -1,8 +1,19 @@
-function About() {
+import pic2 from '../images/pexels-asadphoto-169198.jpg'
+
+ function About() {
     
     return(
-        <div>
-
+        <div className="about">
+<div className="service">
+    <div className="wed1">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.Lorem ipsum dolor sit amet, <br></br>consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br>Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p>
+    </div>
+    <div className="wed2">
+        <img src={pic2} ></img>
+    </div>
+</div>
         </div>
     )
 }
