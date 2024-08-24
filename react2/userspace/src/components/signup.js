e6557ca0b3e7d032a27457bfb69d728961a59d62
mark <ngarimark27@gmail.com>
2024-08-24 23:56:21 +0300

    created sign up form

--- react2/userspace/src/components/signup.js
+++ react2/userspace/src/components/signup.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 function Signup  () {
     return(
         <div>
-
+<div>
+    <form>
+    <label> User Name : </label>
+     <input type='string' name='username'  placeholder='enter user name' required />
+     <label> Password : </label>
+     <input type='password' name='password'  placeholder='enter password' required /><br></br>
+     <label>Email : </label>
+     <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </form>
+    <p>already have an account <a>Login</a></p>
+</div>
         </div>
     )
     
