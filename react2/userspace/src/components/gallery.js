3f75048baadf73bca5887431a88590b09e9fc4c6
mark <ngarimark27@gmail.com>
2024-08-24 19:22:02 +0300

    creeated a filter on the array

--- react2/userspace/src/components/gallery.js
+++ react2/userspace/src/components/gallery.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const images=[
     {id:'1',imageName:'corporate1.jpeg',tag:'corporate'},
-    {id:'2',imageName:'social1.jpeg',tag:'corporate'},
-    {id:'3',imageName:'wedd1.jpg',tag:'corporate'},
+    {id:'2',imageName:'social1.jpeg',tag:'social'},
+    {id:'3',imageName:'wedd1.jpg',tag:'wedding'},
     {id:'4',imageName:'corporate2.jpg',tag:'corporate'},
-    {id:'5',imageName:'social2.jpeg',tag:'corporate'},
-    {id:'6',imageName:'wedd2.jpg',tag:'corporate'},
+    {id:'5',imageName:'social2.jpeg',tag:'social'},
+    {id:'6',imageName:'wedd2.jpg',tag:'wedding'},
     {id:'7',imageName:'corporate3.jpg',tag:'corporate'},
-    {id:'8',imageName:'social3.jpg',tag:'corporate'},
-    {id:'9',imageName:'wedd3.jpeg',tag:'corporate'},
+    {id:'8',imageName:'social3.jpg',tag:'social'},
+    {id:'9',imageName:'wedd3.jpeg',tag:'wedding'},
     {id:'10',imageName:'corporate4.jpg',tag:'corporate'},
-    {id:'11',imageName:'spocial4.jpg',tag:'corporate'},
-    {id:'12',imageName:'wedd4.jpeg',tag:'corporate'},
+    {id:'11',imageName:'social4.jpg',tag:'social'},
+    {id:'12',imageName:'wedd4.jpeg',tag:'wedding'},
     {id:'13',imageName:'corporate5.jpeg',tag:'corporate'},
-    {id:'14',imageName:'spocial5.jpg',tag:'corporate'},
-    {id:'15',imageName:'wedd5.jpeg',tag:'corporate'},
+    {id:'14',imageName:'social5.jpg',tag:'social'},
+    {id:'15',imageName:'wedd5.jpeg',tag:'wedding'},
     {id:'16',imageName:'corporate6.jpeg',tag:'corporate'},
-    {id:'17',imageName:'social6.jpeg',tag:'corporate'},
-    {id:'18',imageName:'wedd6.jpeg',tag:'corporate'},
+    {id:'17',imageName:'social6.jpeg',tag:'social'},
+    {id:'18',imageName:'wedd6.jpeg',tag:'wedding'},
     {id:'19',imageName:'corporate7.jpeg',tag:'corporate'},
-    {id:'20',imageName:'social7.jpeg',tag:'corporate'},
-    {id:'21',imageName:'wedd7.jpeg',tag:'corporate'},
+    {id:'20',imageName:'social7.jpeg',tag:'social'},
+    {id:'21',imageName:'wedd7.jpeg',tag:'wedding'},
     {id:'22',imageName:'corporate8.jpeg',tag:'corporate'},
-    {id:'23',imageName:'social8.jpeg',tag:'corporate'},
-    {id:'24',imageName:'wedd8.jpeg',tag:'corporate'},
+    {id:'23',imageName:'social8.jpeg',tag:'social'},
+    {id:'24',imageName:'wedd8.jpeg',tag:'wedding'},
     {id:'25',imageName:'corporate9.jpeg',tag:'corporate'},
 ]
 function Gallery (){
-   
+    // used react hooks
+   const[tag,setTag]= useState('social')
+   //storing filted tags
+   const [filterd,setFilterd] =useState([])
+   //monituring tag if it changes it shows filterd tag
+   useEffect(() => {
+tag === 'all' ? setFilterd(images) : setFilterd(images.filter(image => image.tag === tag))
+   },[tag])
+
 return(
     <div className='gallery'>
         <div>
-{images.map(image => <div>{image.imageName}</div>)}
+            {/* looping through all images */}
+{filterd.map(image => <div>{image.imageName}</div>)}
         </div>
 
     </div>
