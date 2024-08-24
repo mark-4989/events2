11b74ae28329ce3af5469587b3f8dfaae613322c
mark <ngarimark27@gmail.com>
2024-08-24 18:26:06 +0300

    added image to gallery

--- react2/userspace/src/components/gallery.js
+++ react2/userspace/src/components/gallery.js
@@ -1,6 +1,15 @@
+import React from "react";
+
+const images=[
+    {id:'1',imageName:'corporate1.jpeg',tag:'corporate'}
+]
 function Gallery (){
+   
 return(
-    <div>
+    <div className='gallery'>
+        <div>
+{images.map(image => <div>{image.imageName}</div>)}
+        </div>
 
     </div>
 )
