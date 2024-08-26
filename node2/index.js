0c7e7407fcf884fab93f4e7922f46c5ed88f7e4f
mark <ngarimark27@gmail.com>
2024-08-27 00:11:36 +0300

    restarted and setup server

--- node2/index.js
+++ node2/index.js
@@ -1,42 +1,11 @@
-const express =require('express');
-const mongoose =require('mongoose')
-const userModel = require('./models/userModel')
-const cors = require('cors')
-
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+// ensuring post is ok
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-var corOptions = {
-  origin: 'http://localhost:3000'
-};
-app.use(cors(corOptions));
-// app.use(act)
-// require("./routes/clientRoutes")
-app.use(express.json());
-//require('./helper/mongo')
-// app.use(express.urlencoded({ extended: true }));
-
- mongoose.connect('mongodb://localhost:27017/events/',console.log('connected to db'))
- app.post("/createClient",(req,res)=>{
-    userModel.create(req.body)
-    .then(clients => res.json(clients))
-    .catch(err =>console.log(err.message))
-    })
-const PORT = process.env.PORT || 3001;
-// error handler
-app.use((req, res, next) => {
-    const err = new Error("not found");
-    err.status = 404;
-    next(err);
-});
-// error handler
-app.use((req, res, next) => {
-    res.status(err.status || 500);
-    res.send(error, {
-        status: err.status || 500,
-        message: err.message,
-    });
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
