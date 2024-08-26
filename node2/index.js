538d1c66df5f0e879a4c48968cce3c5ab0afb67a
mark <ngarimark27@gmail.com>
2024-08-27 00:38:16 +0300

    connection to db successful

--- node2/index.js
+++ node2/index.js
@@ -9,3 +9,12 @@ const app = express();
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 
+// connection string
+const CONNECTION_URL='mongodb://localhost:27017/';
+const PORT= process.env.PORT || 3001
+
+// checking connection
+mongoose.connect(CONNECTION_URL)
+.then(()=> app.listen(PORT,()=>console.log(`Server is running on port: ${PORT}`)))
+.catch((error)=>console.log(error.message))
+
