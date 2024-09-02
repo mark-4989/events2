eccab29ef5762991862000e42322a22d0fd558d9
mark <ngarimark27@gmail.com>
2024-09-03 00:34:07 +0300

    styled  card in about

--- react2/userspace/src/components/about.js
+++ react2/userspace/src/components/about.js
@@ -32,6 +32,7 @@ import pic2 from '../picture/pexels-asadphoto-169198.jpg'
     </div>
     
 <div className='glass'>
+    <h4>a little about us:</h4>
     <p>     Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.Lorem ipsum dolor sit amet, <br></br>consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br>Nulla vitae elit libero, a pharetra augue mollis interdum.<br></br> Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
 </div>
   
