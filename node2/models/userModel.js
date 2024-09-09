5a2a4ba428692f104ed1b1821df6858f5926d168
mark <ngarimark27@gmail.com>
2024-09-09 22:16:38 +0300

    readjusted the date data type to take date only

--- node2/models/userModel.js
+++ node2/models/userModel.js
@@ -5,8 +5,21 @@ import mongoose from "mongoose"
     email:String,
     phone:Number,
     eventtype:String,
-    siku:Date,
+    siku :{
+      type: String,
+      match: /^\d{4}-\d{2}-\d{2}$/,
+      required: true
+    },
     guestcount:Number
  })
+
+ // Define a pre-save middleware to format the date
+ clientSchema.pre('save', function(next) {
+   if (this.siku instanceof Date) {
+       this.siku = this.siku.toISOString().split('T')[0];
+   }
+   next();
+});
+
  const clientModel =mongoose.model("clientmodel",clientSchema)
  export default clientModel;
--- react2/userspace/src/components/update.js
+++ react2/userspace/src/components/update.js
@@ -20,7 +20,16 @@ function Update(){
 
   useEffect(()=>{
     axios.get('http://localhost:3001/api/'+id)
-    .then(res =>console.log(res))
+    .then(res =>{console.log(res)
+      setFirst(res.data.firstname)
+      setLast(res.data.lastname)
+      setEmail(res.data.email)
+      setPhone(res.data.phone)
+      setEvent(res.data.eventtype)
+      setSiku(res.data.siku)
+      setGuest(res.data.guestcount)
+
+    })
     .catch(err =>console.log(err))
 },[] )
     return(
@@ -28,19 +37,19 @@ function Update(){
         <Form className='forms' >
           <h2>Enter Your Details Here</h2>
          <label>First Name : </label>
-         <input type='' name='firstname'  placeholder='enter first name' required />
+         <input type='' name='firstname'  placeholder='enter first name' required value={firstname} />
          <label>Last Name : </label>
-         <input type='' name='lastname'  placeholder='enter last name' required /><br></br>
+         <input type='' name='lastname'  placeholder='enter last name' required value={lastname} /><br></br>
          <label>Email : </label>
-         <input type='' name='email'  placeholder='enter email' required/>
+         <input type='' name='email'  placeholder='enter email' required value={email}/>
          <label>Phone Number : </label>
-         <input type='Number' name='phone'  placeholder='enter phone number' required /><br></br>
+         <input type='Number' name='phone'  placeholder='enter phone number' required value={phone} /><br></br>
          <label>Event Type : </label>
-         <input type='' name='eventtype'  placeholder='enter type of event' required />
+         <input type='' name='eventtype'  placeholder='enter type of event' required value={eventtype} />
          <label>Date : </label>
-         <input type='Date' name='siku'  placeholder='enter event date' require /><br></br>
+         <input type='Date' name='siku'  placeholder='enter event date' required value={siku} /><br></br>
          <label>Guest Count : </label>
-         <input type='Number' name='guestcount'  placeholder='enter number of guest' required/><br></br>
+         <input type='Number' name='guestcount'  placeholder='enter number of guest' required value={guestcount}/><br></br>
           <Button variant="primary" type="submit">
             Submit
           </Button>
